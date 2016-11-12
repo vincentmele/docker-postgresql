a0175318a8588e4644540d025cc51e4aa9cc691b
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-11-12 20:44:04 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@
 /runssh.sh
 /usr/sbin/sshd
 
+#cron
+/runcron.sh
+
 set_listen_addresses() {
 	sedEscapedValue="$(echo "$1" | sed 's/[\/&]/\\&/g')"
 	sed -ri "s/^#?(listen_addresses\s*=\s*)\S+/\1'$sedEscapedValue'/" "$PGDATA/postgresql.conf"
